3a2c0d9dc24096a8f8a2c649bd11010fdb8a7d21
M6code <benjamynfrank101@gmail.com>
2021-07-13 15:51:07 +0100

    Create location model

--- trade_fair/tradefair/models.py
+++ trade_fair/tradefair/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+# CREATE TABLE `location` (
+#   `location_id` int NOT NULL AUTO_INCREMENT,
+#   `latitude` decimal(8,6) NOT NULL,
+#   `longitude` decimal(8,6) NOT NULL,
+#   `address` varchar(120) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+#   PRIMARY KEY (`location_id`)
+# ) 
+
 # Create your models here.
+class Location(models.Model):
+    location_id = models.IntegerField(primary_key=True)
+    latitude = models.DecimalField(max_digits=8, decimal_places=6)
+    longitude = models.DecimalField(max_digits=8, decimal_places=6)
+    address = models.CharField(max_length=255, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.location_id} - {self.address}"
